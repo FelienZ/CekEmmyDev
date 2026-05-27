b647dccf598d2ed218033a64095a4d5425d3e6f7
FelienZ <rajabfadh@gmail.com>
2026-05-27 12:34:41 +0700

    feat: Create Initial Endpoint & services

---
+++ src/orders/controllers/orders.controller.ts
@@ -0,0 +1,11 @@
+import { Controller, Get } from '@nestjs/common';
+import { OrdersService } from '../services/orders.service';
+
+@Controller('orders')
+export class OrdersController {
+  constructor(private readonly ordersService: OrdersService) {}
+  @Get()
+  getHello(): string {
+    return this.ordersService.getHello();
+  }
+}
---
+++ src/orders/orders.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { OrdersController } from './controllers/orders.controller';
+import { OrdersService } from './services/orders.service';
+
+@Module({
+  imports: [],
+  controllers: [OrdersController],
+  providers: [OrdersService],
+})
+export class OrdersModule {}
