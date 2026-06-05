f59b81899c3cff53b401195265d6902c662459bc
FelienZ <rajabfadh@gmail.com>
2026-06-05 17:24:44 +0700

    feat: create Http layer for Product Entity

---
+++ src/products/controllers/products.controller.ts
@@ -0,0 +1,52 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { ProductService } from '../services/product.service';
+import { GetProductResponseDto } from '../dto/get-response.dto';
+import { CreateProductDto } from '../dto/create-product.dto';
+
+@Controller('products')
+export class ProductsController {
+  constructor(private productService: ProductService) {}
+
+  @Get()
+  async findAllProducts(): Promise<GetProductResponseDto[]> {
+    return await this.productService.findAllProducts();
+  }
+
+  @Get('/:id')
+  async findProductById(
+    @Param('id') id: string,
+  ): Promise<GetProductResponseDto | string> {
+    return await this.productService.findProductById(id);
+  }
+
+  @Post()
+  async createProduct(
+    @Body() payload: CreateProductDto,
+  ): Promise<{ message: string; data: { id: string } }> {
+    const id = await this.productService.createProduct(payload);
+    return { message: 'Product created successfully', data: { id } };
+  }
+
+  @Put('/:id')
+  async updateProduct(
+    @Param('id') id: string,
+    @Body() payload: CreateProductDto,
+  ): Promise<{ message: string }> {
+    await this.productService.updateProduct(id, payload);
+    return { message: 'Product updated successfully' };
+  }
+
+  @Delete('/:id')
+  async deleteProduct(@Param('id') id: string): Promise<{ message: string }> {
+    await this.productService.deleteProduct(id);
+    return { message: 'Product deleted successfully' };
+  }
+}
