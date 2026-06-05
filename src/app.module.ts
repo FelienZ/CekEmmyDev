883cde95eb116f988d078f69f4c817b01fac30ab
FelienZ <rajabfadh@gmail.com>
2026-06-05 17:29:29 +0700

    feat: wire all module in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,17 @@
 import { Module } from '@nestjs/common';
 import { OrdersModule } from './orders/orders.module';
+import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from 'prisma/prisma.module';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [OrdersModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    PrismaModule,
+    OrdersModule,
+    ProductsModule,
+  ],
 })
 export class AppModule {}
