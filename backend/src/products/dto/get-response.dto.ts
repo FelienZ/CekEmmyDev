e8ce290fcb19b786924af6eba811769d093a8b38
FelienZ <rajabfadh@gmail.com>
2026-07-20 14:07:52 +0700

    refactor(products/dto): adjust dto response for get product & productCategories

--- backend/src/products/dto/get-response.dto.ts
+++ backend/src/products/dto/get-response.dto.ts
@@ -1,8 +1,17 @@
 export class GetProductResponseDto {
   id!: string;
+  slug!: string | null;
   name!: string;
   price!: number;
   stock!: number;
+  description!: string | null;
   categoryId!: string;
   isAvailable!: boolean;
+}
+
+export class GetProductCategoriesDto {
+  categoryId!: string;
+  name!: string;
+  description!: string | null;
+  products!: GetProductResponseDto[];
 }
