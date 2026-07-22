29e34f880b7b1b9c6dd39ec36ba5e5bfe2ed7164
FelienZ <rajabfadh@gmail.com>
2026-07-22 21:44:23 +0700

    feat(utils): Create queryClient Instance & Theme Provider

---
+++ frontend/src/lib/utils/statusConfig.ts
@@ -0,0 +1,12 @@
+import { OrderStatus, PaymentStatus } from "@/types/order";
+import { CircleCheckIcon, CircleX, LoaderIcon } from "lucide-react";
+
+export const statusConfig = {
+  [OrderStatus.COMPLETED]: { className: "bg-primary text-white", icon: CircleCheckIcon },
+  [OrderStatus.READY]: { className: "bg-primary text-white", icon: CircleCheckIcon },
+  [OrderStatus.PREPARING]: { className: "bg-blue-500 text-white", icon: LoaderIcon },
+  [OrderStatus.PENDING]: { className: "bg-blue-500 text-white", icon: LoaderIcon },
+  [OrderStatus.CANCELLED]: { className: "bg-destructive text-white", icon: CircleX },
+  [PaymentStatus.PAID]: { className: "bg-primary/80 text-white", icon: CircleCheckIcon },
+  [PaymentStatus.UNPAID]: { className: "bg-destructive/80 text-white", icon: CircleX },
+}
