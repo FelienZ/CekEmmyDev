ff1b02618d52cb9f38b66a334a500992e5e34974
FelienZ <rajabfadh@gmail.com>
2026-07-27 23:24:15 +0700

    feat(frontend/utils): Update new config for new payment status option

--- frontend/src/lib/utils/statusConfig.ts
+++ frontend/src/lib/utils/statusConfig.ts
@@ -1,5 +1,5 @@
 import { OrderStatus, PaymentStatus } from "@/types/order";
-import { CircleCheckIcon, CircleX, LoaderIcon } from "lucide-react";
+import { CircleCheckIcon, CirclePercent, CircleX, LoaderIcon } from "lucide-react";
 
 export const statusConfig = {
   [OrderStatus.COMPLETED]: { className: "bg-primary text-white", icon: CircleCheckIcon },
@@ -9,4 +9,5 @@ export const statusConfig = {
   [OrderStatus.CANCELLED]: { className: "bg-destructive text-white", icon: CircleX },
   [PaymentStatus.PAID]: { className: "bg-primary/80 text-white", icon: CircleCheckIcon },
   [PaymentStatus.UNPAID]: { className: "bg-destructive/80 text-white", icon: CircleX },
+  [PaymentStatus.PARTIAL]: { className: "bg-muted/80 text-white", icon: CirclePercent },
 }
