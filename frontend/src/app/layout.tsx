22f02cabb95531fabaa5b958d8f049df9c815992
FelienZ <rajabfadh@gmail.com>
2026-07-27 23:27:45 +0700

    feat(frontend/app): Update css Config & Base Layout using Shadcn Blocks

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,20 +1,12 @@
 import type { Metadata } from "next";
 import { Lora, Outfit } from "next/font/google";
 import "./globals.css";
-import { ThemeProvider } from "@/components/theme-provider";
+import { ThemeProvider } from "@/lib/theme-provider";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 import { SiteHeader } from "@/components/site-header";
 import { AppSidebar } from "@/components/app-sidebar";
-
-/* const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-}); */
+import QueryProvider from "@/lib/queryProvider";
+import { Toaster } from "@/components/ui/sonner";
 
 const outfit = Outfit({
   variable: "--font-sans",
@@ -28,8 +20,16 @@ const lora = Lora({
   weight: ["600", "700"],
 });
 
+const CompanyProfile = {
+  name: "Pempek Cek Emmy",
+  logoUrl: "/Cek_Emmy-logo-clean.png",
+};
+
 export const metadata: Metadata = {
   title: "Pempek Cek Emmy",
+  icons: {
+    icon: CompanyProfile.logoUrl,
+  },
   description: "Website Kedai Pempek Cek Emmy Khas Palembang",
 };
 
@@ -44,34 +44,45 @@ export default function RootLayout({
       className={`${outfit.variable} ${lora.variable} h-full antialiased`}
       suppressHydrationWarning
     >
-      <body className="min-h-full flex flex-col">
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          <SidebarProvider
-            style={
-              {
-                "--sidebar-width": "calc(var(--spacing) * 72)",
-                "--header-height": "calc(var(--spacing) * 12)",
-              } as React.CSSProperties
-            }
+      <body className="min-h-full flex flex-col bg-background text-foreground">
+        <QueryProvider>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            // disableTransitionOnChange
           >
-            <AppSidebar variant="sidebar" />
-            <SidebarInset>
-              <SiteHeader name="Cek Emmy" />
-              <div className="flex flex-1 flex-col">
-                <div className="@container/main flex flex-1 flex-col gap-2">
-                  <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
-                    {children}
+            <SidebarProvider
+              defaultOpen={false}
+              style={
+                {
+                  "--sidebar-width": "calc(var(--spacing) * 72)",
+                  "--header-height": "calc(var(--spacing) * 12)",
+                } as React.CSSProperties
+              }
+            >
+              <AppSidebar
+                name={CompanyProfile.name}
+                logoUrl={CompanyProfile.logoUrl}
+                props={{ variant: "sidebar" }}
+              />
+              <SidebarInset>
+                <SiteHeader
+                  name={CompanyProfile.name}
+                  logoUrl={CompanyProfile.logoUrl}
+                />
+                <div className="flex flex-1 flex-col">
+                  <div className="@container/main flex flex-1 flex-col gap-2">
+                    <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+                      {children}
+                      <Toaster position="bottom-right" richColors />
+                    </div>
                   </div>
                 </div>
-              </div>
-            </SidebarInset>
-          </SidebarProvider>
-        </ThemeProvider>
+              </SidebarInset>
+            </SidebarProvider>
+          </ThemeProvider>
+        </QueryProvider>
       </body>
     </html>
   );
