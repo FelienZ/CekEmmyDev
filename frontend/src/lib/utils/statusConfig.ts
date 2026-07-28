5ed3a2a7fa60ede907894679bd780e9e6b1f120f
FelienZ <rajabfadh@gmail.com>
2026-07-28 22:32:32 +0700

    feat(frontend/app): Update Theme Config for light/dark Mode and statusConfig

--- frontend/src/lib/utils/statusConfig.ts
+++ frontend/src/lib/utils/statusConfig.ts
@@ -2,12 +2,12 @@ import { OrderStatus, PaymentStatus } from "@/types/order";
 import { CircleCheckIcon, CirclePercent, CircleX, LoaderIcon } from "lucide-react";
 
 export const statusConfig = {
-  [OrderStatus.COMPLETED]: { className: "bg-primary text-white", icon: CircleCheckIcon },
-  [OrderStatus.READY]: { className: "bg-primary text-white", icon: CircleCheckIcon },
-  [OrderStatus.PREPARING]: { className: "bg-blue-500 text-white", icon: LoaderIcon },
-  [OrderStatus.PENDING]: { className: "bg-blue-500 text-white", icon: LoaderIcon },
-  [OrderStatus.CANCELLED]: { className: "bg-destructive text-white", icon: CircleX },
-  [PaymentStatus.PAID]: { className: "bg-primary/80 text-white", icon: CircleCheckIcon },
-  [PaymentStatus.UNPAID]: { className: "bg-destructive/80 text-white", icon: CircleX },
-  [PaymentStatus.PARTIAL]: { className: "bg-muted/80 text-white", icon: CirclePercent },
+  [OrderStatus.COMPLETED]: { className: "bg-primary text-primary-foreground", icon: CircleCheckIcon, val: "Selesai" },
+  [OrderStatus.READY]: { className: "bg-success/80 text-success-foreground", icon: CircleCheckIcon, val: "Sudah Siap" },
+  [OrderStatus.PREPARING]: { className: "bg-info text-info-foreground", icon: LoaderIcon, val: "Mempersiapkan" },
+  [OrderStatus.PENDING]: { className: "bg-muted text-muted-foreground", icon: LoaderIcon, val: "Pending" },
+  [OrderStatus.CANCELLED]: { className: "bg-destructive text-destructive-foreground", icon: CircleX, val: "Dibatalkan" },
+  [PaymentStatus.PAID]: { className: "bg-primary/80 text-primary-foreground", icon: CircleCheckIcon, val: "Lunas" },
+  [PaymentStatus.UNPAID]: { className: "bg-destructive/80 text-destructive-foreground", icon: CircleX, val: "Belum Bayar" },
+  [PaymentStatus.PARTIAL]: { className: "bg-muted/80 text-muted-foreground", icon: CirclePercent, val: "Panjar" },
 }
