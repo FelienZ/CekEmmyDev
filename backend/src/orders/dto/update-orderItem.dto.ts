64e33cf35d4a532d4e728ccc2726b4b1f6e3cbef
FelienZ <rajabfadh@gmail.com>
2026-08-01 14:57:18 +0700

    refactor(backend/orders/dto): change DTO of updateOrderItem to extend CreateOrderItem

--- backend/src/orders/dto/update-orderItem.dto.ts
+++ backend/src/orders/dto/update-orderItem.dto.ts
@@ -1,14 +1,7 @@
-import { IsInt, Min, IsString, IsNotEmpty, IsOptional } from 'class-validator';
+import { IsInt, Min, IsOptional } from 'class-validator';
+import { CreateOrderItemDto } from './create-orderItem.dto';
 
-export class UpdateOrderItemDto {
-  @IsString()
-  @IsNotEmpty({ message: 'ID produk tidak boleh kosong' })
-  productId!: string;
-
-  @IsInt()
-  @Min(1, { message: 'Jumlah pesanan tidak boleh kurang dari 1' })
-  quantity!: number;
-
+export class UpdateOrderItemDto extends CreateOrderItemDto {
   @IsInt()
   @Min(0, { message: 'Progress tidak boleh kurang dari 0' })
   @IsOptional()
