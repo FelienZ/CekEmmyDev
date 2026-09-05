3f85a0ecf01a70280483fd387d70e33f6e1e8cd7
FelienZ <rajabfadh@gmail.com>
2026-09-05 16:47:58 +0700

    feat(backend/app): Wire Finance Module into app global module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,6 +3,7 @@ import { OrdersModule } from './orders/orders.module';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from 'prisma/prisma.module';
 import { ProductsModule } from './products/products.module';
+import { FinanceModule } from './finance/finance.module';
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { ProductsModule } from './products/products.module';
     PrismaModule,
     OrdersModule,
     ProductsModule,
+    FinanceModule,
   ],
 })
 export class AppModule {}
